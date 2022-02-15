9010f95c2b989c7e8b51f4fd79b57cda5e0d77d3
shinarasheed <rasheedshinaopeyemi@gmail.com>
2022-02-15 18:29:12 +0100

    add noinventory screen

--- src/screens/AccountScreen/NoInventory.js
+++ src/screens/AccountScreen/NoInventory.js
@@ -1,4 +1,11 @@
-import { View, Text, Image, TouchableOpacity, Pressable } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  StatusBar,
+  TouchableOpacity,
+  Pressable,
+} from "react-native";
 import React from "react";
 
 import appTheme from "../../constants/theme";
@@ -11,76 +18,109 @@ const NoInventory = () => {
   return (
     <View
       style={{
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
         flex: 1,
-        backgroundColor: appTheme.COLORS.mainBackground,
-        paddingHorizontal: 20,
       }}
     >
       <View
         style={{
-          justifyContent: "center",
-          alignItems: "center",
-          marginBottom: 40,
-        }}
-      >
-        <Image source={icons.NoProductIcon} />
-        <Text
-          style={{
-            fontSize: 15,
-            fontFamily: "Gilroy-Medium",
-            marginTop: 10,
-          }}
-        >
-          You do not have any products in your store yet
-        </Text>
-      </View>
-
-      <TouchableOpacity
-        onPress={() => navigation.navigate(Routes.ADDPRODUCTS_SCREEN)}
-        style={{
+          backgroundColor: appTheme.COLORS.mainRed,
+          paddingLeft: 20,
           flexDirection: "row",
           alignItems: "center",
-          borderWidth: 1,
-          width: "100%",
-          justifyContent: "center",
-          borderRadius: 5,
-          borderColor: appTheme.COLORS.borderGRey1,
-          paddingVertical: 10,
+          paddingBottom: 10,
+          paddingTop: StatusBar.currentHeight * 1.5,
         }}
       >
         <Pressable
           style={{
-            width: 25,
-            height: 25,
-            backgroundColor: appTheme.COLORS.mainYellow,
-            borderRadius: 5,
-            justifyContent: "center",
+            flexDirection: "row",
             alignItems: "center",
-            marginRight: 5,
           }}
+          onPress={() => navigation.goBack()}
         >
+          <Image source={icons.backIcon} style={{ marginRight: 18 }} />
           <Text
             style={{
+              fontFamily: "Gilroy-Bold",
               color: appTheme.COLORS.white,
+              fontSize: 15,
             }}
           >
-            +
+            Products
           </Text>
         </Pressable>
-
-        <Text
+      </View>
+      <View
+        style={{
+          backgroundColor: appTheme.COLORS.mainBackground,
+          flex: 1,
+          justifyContent: "center",
+          paddingHorizontal: 20,
+        }}
+      >
+        <View
           style={{
-            color: appTheme.COLORS.black,
-            fontFamily: "Gilroy-Medium",
-            fontSize: 18,
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: 40,
           }}
         >
-          Add Products
-        </Text>
-      </TouchableOpacity>
+          <Image source={icons.NoProductIcon} />
+          <Text
+            style={{
+              fontSize: 15,
+              fontFamily: "Gilroy-Medium",
+              marginTop: 10,
+            }}
+          >
+            You do not have any products in your store yet
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate(Routes.ADDPRODUCTS_SCREEN)}
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            borderWidth: 1,
+            width: "100%",
+            justifyContent: "center",
+            borderRadius: 5,
+            borderColor: appTheme.COLORS.borderGRey1,
+            paddingVertical: 10,
+          }}
+        >
+          <Pressable
+            style={{
+              width: 25,
+              height: 25,
+              backgroundColor: appTheme.COLORS.mainYellow,
+              borderRadius: 5,
+              justifyContent: "center",
+              alignItems: "center",
+              marginRight: 5,
+            }}
+          >
+            <Text
+              style={{
+                color: appTheme.COLORS.white,
+              }}
+            >
+              +
+            </Text>
+          </Pressable>
+
+          <Text
+            style={{
+              color: appTheme.COLORS.black,
+              fontFamily: "Gilroy-Medium",
+              fontSize: 18,
+            }}
+          >
+            Add Products
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
