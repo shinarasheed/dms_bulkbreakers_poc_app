0f0edcbd86a216d2ee9fac71d2599e4eed9c035e
shinarasheed <rasheedshinaopeyemi@gmail.com>
2022-02-16 20:12:03 +0100

    seperate products

--- src/components/account/Header.js
+++ src/components/account/Header.js
@@ -7,37 +7,82 @@ import {
   View,
   Pressable,
   Image,
+  TouchableOpacity,
 } from "react-native";
 
 import appTheme from "../../constants/theme";
 import { icons } from "../../constants";
 
-export const Header = ({ single, orderId, title }) => {
+export const Header = ({ title }) => {
   const navigation = useNavigation();
   return (
     <View
       style={{
         backgroundColor: appTheme.COLORS.mainRed,
         paddingTop: StatusBar.currentHeight * 1.8,
-        flexDirection: "row",
-        alignItems: "center",
         paddingBottom: 15,
         paddingHorizontal: 20,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
       }}
     >
-      <Pressable onPress={() => navigation.goBack()}>
-        <Image source={icons.backIcon} style={{ marginRight: 18 }} />
-      </Pressable>
-
-      <Text
+      <View
         style={{
-          fontSize: 16,
-          color: appTheme.COLORS.white,
-          fontFamily: "Gilroy-Bold",
+          flexDirection: "row",
+          alignItems: "center",
         }}
       >
-        {title}
-      </Text>
+        <Pressable onPress={() => navigation.goBack()}>
+          <Image source={icons.backIcon} style={{ marginRight: 18 }} />
+        </Pressable>
+
+        <Text
+          style={{
+            fontSize: 16,
+            color: appTheme.COLORS.white,
+            fontFamily: "Gilroy-Bold",
+          }}
+        >
+          {title}
+        </Text>
+      </View>
+
+      <TouchableOpacity
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        <Pressable
+          style={{
+            width: 20,
+            height: 20,
+            backgroundColor: appTheme.COLORS.white,
+            borderRadius: 5,
+            justifyContent: "center",
+            alignItems: "center",
+            marginRight: 5,
+          }}
+        >
+          <Text
+            style={{
+              color: appTheme.COLORS.mainRed,
+            }}
+          >
+            +
+          </Text>
+        </Pressable>
+
+        <Text
+          style={{
+            color: appTheme.COLORS.white,
+            fontFamily: "Gilroy-Bold",
+          }}
+        >
+          ADD
+        </Text>
+      </TouchableOpacity>
     </View>
   );
 };
